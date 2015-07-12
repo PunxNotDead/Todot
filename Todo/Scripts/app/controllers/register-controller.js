db6591aeda4ff83bc2dbfe2e11d96205ab685fd5
Mikhail Ziankevich <zin@zame-dev.org>
2015-07-12 21:37:45 +0300

    Angular controller

---
+++ Todo/Scripts/app/controllers/register-controller.js
@@ -0,0 +1,6 @@
+﻿
+angular.module('TodoApp').controller('RegisterController', ['$scope', 'UserFactory', function($scope, UserFactory) {
+    $scope.register = function() {
+        UserFactory.post($scope.user);
+    };
+}])
